03ddd38f5cacb58c2c2cc9ebd65ffcb455867189
Parthiban <parthiban@macpc211108.local>
2017-10-24 16:56:58 +0530

    Added Bootstrap and removed materal design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AlertModule } from 'ngx-bootstrap';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    AlertModule.forRoot()
+
   ],
   providers: [],
   bootstrap: [AppComponent]
