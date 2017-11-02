09e8020a05a567c1f9ba63905083bd9e71fc7809
Parthiban <parthiban@macpc211108.local>
2017-11-02 18:33:46 +0530

    Initial  Dashboard commit with Bootstrap 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { AlertModule } from 'ngx-bootstrap';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { ButtonsModule } from 'ngx-bootstrap';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -12,7 +13,8 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
-    AlertModule.forRoot()
+    BsDropdownModule.forRoot(),
+    ButtonsModule.forRoot()
 
   ],
   providers: [],
