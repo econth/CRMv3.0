b37ebff538dbec5b252fa14ecc4c8a4b24d4518b
Parthiban <kgparthiban31@gmail.com>
2017-11-06 10:57:15 +0530

    Added bootstrap.js for Testing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
-import { ButtonsModule } from 'ngx-bootstrap';
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
