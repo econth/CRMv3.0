b1c542140e3a0dc5c1cce6aa5a0174fa0e5ebe65
Parthiban <kgparthiban31@gmail.com>
2017-11-29 09:36:05 +0530

    Coding for service - initial commit 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule} from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutes } from './app.routing'
 import { AppComponent } from './app.component';
@@ -8,44 +9,6 @@ import { LoginComponent } from './login/login.component';
 import { MainComponent } from './main/main.component';
 import { EconthHeaderComponent } from './_directives/econth-header/econth-header.component';
 import { EconthSidebarComponent } from './_directives/econth-sidebar/econth-sidebar.component';
-// import { DashboardComponent } from './dashboard/dashboard.component';
-// import { LoginComponent } from './login/login.component';
-// import { ListAppointmentsComponent } from './appointment/list-appointments.component';
-// import { AddEmployeeComponent } from './employee/add-employee.component';
-// import { AddCustomerComponent } from './customer/add-customer.component';
-// import { EconthHeaderComponent } from './_directives/econth-header/econth-header.component';
-// import { EconthSidebarComponent } from './_directives/econth-sidebar/econth-sidebar.component';
-// import { MainComponent } from './main/main.component';
-// import { BookAppointmentComponent } from './appointment/book-appointment.component';
-// import { SearchCustomerComponent } from './customer/search-customer.component';
-// import { InactiveCustomersComponent } from './customer/inactive-customers.component';
-// import { InactiveEmployeesComponent } from './employee/inactive-employees.component';
-// import { SearchEmployeeComponent } from './employee/search-employee.component';
-// import { AccountsComponent } from './accounts/accounts.component';
-// import { ReportsComponent } from './reports/reports.component';
-// import { InvoicesComponent } from './invoices/invoices.component';
-
-// export const ROUTES: Routes = [{
-//   path: 'home', component: MainComponent,
-//   children: [
-//         { path: 'dashboard',component:DashboardComponent},
-//         { path: 'book-appointment', component: BookAppointmentComponent },
-//         { path: 'appointments', component: ListAppointmentsComponent },
-//         { path: 'add-employee', component: AddEmployeeComponent },
-//         { path: 'search-employee', component: SearchEmployeeComponent },
-//         { path: 'inactive-employees', component: InactiveEmployeesComponent },
-//         { path: 'add-customer', component: AddCustomerComponent },
-//         { path: 'search-customer', component: SearchCustomerComponent },
-//         { path: 'inactive-customers', component: InactiveCustomersComponent },
-//         { path: 'accounts',component:AccountsComponent},
-//         { path: 'invoices',component:InvoicesComponent},
-//         { path: 'reports',component:ReportsComponent},
-//         { path: '', redirectTo: 'dashboard', pathMatch: 'full' }
-//       ]
-//     },
-//   { path: 'login', component: LoginComponent },
-//   { path: '', redirectTo: 'login', pathMatch: 'full' }
-// ];
 
 @NgModule({
   declarations: [
@@ -57,6 +20,7 @@ import { EconthSidebarComponent } from './_directives/econth-sidebar/econth-side
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     RouterModule.forRoot(AppRoutes)
 
   ],
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
   pageTitle:  string = 'Login Page';
+  model : any={};
+ 
   constructor() { }
 
   ngOnInit() {
