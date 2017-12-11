055307da50ce1032be17aea72291a0cbb2b3ceb0
Parthiban <kgparthiban31@gmail.com>
2017-12-11 09:01:27 +0530

    Code updates

--- src/app/_directives/econth-header/econth-header.component.ts
+++ src/app/_directives/econth-header/econth-header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../../_service/authentication.service';
 
 @Component({
   selector: 'econth-header',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EconthHeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authenticationService:AuthenticationService) { }
 
-  ngOnInit() {
+  ngOnInit() {  }
+
+  logout(){
+    this.authenticationService.logout();
   }
 
 }
